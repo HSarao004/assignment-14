427cdf134026d3ea0990ea28ac69935f3f464dda
Hsarao2 <149915512+HSarao004@users.noreply.github.com>
2024-07-31 18:42:04 -0500

    c

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,23 +1,22 @@
 module.exports = {
-    transform: {
-      "^.+\\.(t|j)sx?$": "babel-jest"
-    },
-    moduleNameMapper: {
-      "\\.(css|less|scss)$": "identity-obj-proxy"
-    },
-    modulePathIgnorePatterns: ["node_modules", "jest-test-results.json"],
-    testEnvironment: "jest-environment-jsdom",
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-    reporters: [
-      "default",
-      [
-        "jest-html-reporter",
-        {
-          "pageTitle": "Test Report",
-          "outputPath": "./.jest-test-results.json",
-          "includeFailureMsg": true
-        }
-      ]
-    ],
-  };
-  
+  transform: {
+    "^.+\\.(t|j)sx?$": "babel-jest"
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss)$": "identity-obj-proxy"
+  },
+  modulePathIgnorePatterns: ["node_modules", "jest-test-results.json"],
+  testEnvironment: "jest-environment-jsdom",
+  reporters: [
+    "default",
+    [
+      "jest-html-reporter",
+      {
+        "pageTitle": "Test Report",
+        "outputPath": "./.jest-test-results.json",
+        "includeFailureMsg": true
+      }
+    ]
+  ],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"]
+};
