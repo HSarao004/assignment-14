ea045148048dba79f37230f2ba6c69757fd03bc7
Hsarao2 <149915512+HSarao004@users.noreply.github.com>
2024-08-01 13:36:22 -0500

    homepage changes

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -10,7 +10,7 @@ const HomePage = () => {
       <Text content="Harsimran Singh Sarao " />
         <Img src="path/to/your/photo.jpg" alt="Your Photo" />
         <h1>Your Name</h1>
-        <Text content="Your info and a brief introduction." />
+        <Text content="Full Stack Web Developer " />
         <Text content="Let's talk about your project!" />
         <MyButton text="Contact Me" onClick={() => window.location.href = "/contact"} />
       </section>
